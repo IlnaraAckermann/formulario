2039335c45a51c4fe5c49419d5aaa157779fcb2a
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2023-07-22 23:42:27 -0300

    atualizada a validação do email

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,5 @@
 const form = document.querySelector('form')
 const formfields = form.querySelectorAll('input, textarea');
-const emailInput = form.querySelector('#email');
-console.log(emailInput)
-
-
 
 formfields.forEach((e) => {
   e.addEventListener('input', () => {
@@ -11,51 +7,49 @@ formfields.forEach((e) => {
   })
 })
 
-
 function hasText(e) {
-  if (e.value !== '') {
-    e.classList.add('filled')
-    e.classList.remove('notValidated')
-  } else {
-    e.classList.remove('filled')
+    if (e.value !== '') {
+      e.classList.add('filled')
+      e.classList.remove('notValidated')
+    } else {
+      e.classList.remove('filled')
+    }
   }
-}
 
 
 form.addEventListener('submit', function (event) {
-  event.preventDefault();
-  let isFormValid = true;
+    event.preventDefault();
+    let isFormValid = true;
 
 
-  formfields.forEach((field) => {
-    if (field.id === 'email' && !validarEmail(field.value)) {
-      isFormValid = false;
-      field.classList.add('notValidated');
-    } else if (field.value === '') {
-      isFormValid = false;
-      field.classList.add('notValidated');
-    } else {
-      field.classList.remove('notValidated');
+    formfields.forEach((field) => {
+      if (field.id === 'email' && !validarEmail(field.value)) {
+        isFormValid = false;
+        field.classList.add('notValidated');
+        e.classList.remove('filled')
+      } else if (field.value === '') {
+        isFormValid = false;
+        field.classList.add('notValidated');
+      } else {
+        field.classList.remove('notValidated');
+      }
+    });
+
+
+    if (isFormValid) {
+      form.submit();
+      form.reset();
+      formfields.forEach((e) => {
+        e.classList.remove('filled')
+      })
     }
   });
-
-
-  if (isFormValid) {
-    form.submit();
-    form.reset();
-    formfields.forEach((e) => {
-      e.classList.remove('filled')
-    })
-  }
-});
 
 
 function formatarTelefone() {
   const telefoneInput = document.getElementById('telefone');
   let telefone = telefoneInput.value;
-
   telefone = telefone.replace(/\D/g, ''); // Remove caracteres não numéricos
-
   if (telefone.length === 11) {
     telefone = telefone.replace(/^(\d{2})(\d{5})(\d{4})$/, '($1) $2-$3');
   } else if (telefone.length === 10) {
@@ -67,7 +61,7 @@ function formatarTelefone() {
 }
 
 function validarEmail(email) {
-  const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z0-9]{2,}$/;
   return regex.test(email);
 }
 
